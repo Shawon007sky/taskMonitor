97a8889fc49a79b57b4ca726d7193d597cc05763
shawon khan <minhaz.shawon.khan@gmail.com>
2018-12-05 13:54:29 +0600

    complete project

---
+++ TaskManager.MVC5/Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Scheduler.MVC5.Users;
+using TaskManager.MVC5.Models;
+
+namespace TaskManager.MVC5.Controllers
+{
+    public class AccountController : Controller
+    {
+
+
+
+        #region sign in helper
+        private SignInHelper _helper;
+
+        private SignInHelper SignInHelper
+        {
+            get { return _helper ?? (_helper = new SignInHelper(AppUserManagerProvider.GetAppUserManager, AppUserManagerProvider.AuthenticationManager)); }
+        }
+        #endregion
+        AppUserManager userManager;
+
+        public AccountController()
+        {
+          
+            AppUserManagerProvider.GetAppUserManager = userManager;
+        }
+        
+        [HttpGet]
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+    
+        [HttpPost]
+        public async Task<ActionResult> LogOn(LogOnModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var result = await SignInHelper.PasswordSignIn(model.UserName, model.Password, model.RememberMe);
+
+            switch (result)
+            {
+                case "Success":
+                {
+                    return RedirectToAction("Index", "System");
+                }
+                default:
+                {
+                    ModelState.AddModelError("", "The user name or password is incorrect.");
+                        
+                }break;
+                        
+            }
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+            AppUserManagerProvider.AuthenticationManager.SignOut();
+            return RedirectToAction("LogOn", "Account");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var user = new ApplicationUser {UserName = model.UserName, Email = model.Email};
+            var result = await AppUserManagerProvider.GetAppUserManager.CreateAsync(user, model.Password);
+                
+            if (result.Succeeded)
+            {
+                
+                await AppUserManagerProvider.GetAppUserManager.AddToRoleAsync(user.Id, "Employee");
+                await SignInHelper.SignInAsync(user, isPersistent: true, rememderBrowser: false);
+                return RedirectToAction("Index", "System");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ TaskManager.MVC5/Controllers/StaffsController.cs
@@ -0,0 +1,45 @@
+﻿using Scheduler.MVC5.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskManager.MVC5.Models;
+
+namespace TaskManager.MVC5.Controllers
+{
+    public class StaffsController : Controller
+    {
+        private readonly Staff_ManagerEntities staff_ManagerEntities =new Staff_ManagerEntities();
+
+        private AppUserManagerProvider _appUserManagerProvider;
+
+        public AppUserManagerProvider AppUserManagerProvider
+        {
+            get { return _appUserManagerProvider ?? (_appUserManagerProvider = new AppUserManagerProvider()); }
+        }
+
+
+        
+        [Authorize(Roles ="Manager")]
+        // GET: Staff
+        public ActionResult Index()
+        {
+
+            var staffs=staff_ManagerEntities.UserInRoles.Where(x=>x.RoleId== "c5f7b779-cf81-4d0c-b6a1-8d9bedd75418").ToList();
+            return View(staffs);
+        }
+
+
+        public ActionResult DeleteStaff(string id)
+        {
+            var staff = staff_ManagerEntities.Users.FirstOrDefault(x => x.Id == id);
+            staff_ManagerEntities.Users.Remove(staff);
+            staff_ManagerEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
